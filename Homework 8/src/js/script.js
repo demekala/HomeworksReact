f28e03e5f6383d741dab36540ba41312b61fdd3c
demekala <98095040+demekala@users.noreply.github.com>
2023-12-25 18:12:00 +0400

    homework 8

---
+++ Homework 8/src/js/script.js
@@ -0,0 +1,31 @@
+let nums = [];
+let arr = [15, 53, 22, 198, 10, 28, 16, 70, 33, 951];
+do {
+  var salary = parseInt(window.prompt("enter your salary", ""), 10);
+} while (isNaN(salary) || salary < 1);
+
+for (let i = 0; i < getRandomInt(20); i++) {
+  nums[i] = getRandomInt(100);
+}
+
+document.write("random numbers: ");
+
+for (let i = 0; i < nums.length; i++) {
+  document.write(nums[i] + ", ");
+}
+
+document.write("<br>");
+document.write("odd numbers: ");
+
+arr.forEach((number) => {
+  if (number % 2 == 0) document.write(number + ", ");
+});
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+if (salary < 1000) alert("you are broke");
+else if (salary < 2000) alert("you have approximately average salary");
+else if (salary < 10000) alert("you are almost rich");
+else alert("you are rich af");
